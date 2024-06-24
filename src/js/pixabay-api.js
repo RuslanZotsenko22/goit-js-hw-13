426547b59ace30750820dbe89cfe5b5984ca34a6
mrBidone <ab.bz@ukr.net>
2024-06-25 01:44:28 +0200

    Add completed task with styles

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -4,17 +4,18 @@ const axios = Axios.create({
   baseURL: 'https://pixabay.com/api/',
   params: {
     key: '44449535-a1df9548b4e4ca826019364d7',
-    q: '',
     image_type: 'photo',
     orientation: 'horizontal',
     safesearсh: true,
+    per_page: '15',
   },
 });
 
-export async function getImage(imgName) {
+export async function getImage(imgName, currentPage) {
   const res = await axios.get('', {
     params: {
       q: imgName,
+      page: currentPage,
     },
   });
 
--- src/main.js
+++ src/main.js
@@ -9,42 +9,76 @@ import { Axios } from 'axios';
 const searchForm = document.querySelector('.search-form');
 const markupList = document.querySelector('.gallery-list');
 const loader = document.querySelector('.loader');
+const btnLoadmore = document.querySelector('.loadMore-button');
+
+let inputValue = '';
+let currentPage = 1;
+let maxPage = 1;
+const perPage = 15;
 
 searchForm.addEventListener('submit', async event => {
   event.preventDefault();
 
-  const inputValue = event.target.elements.search.value;
+  inputValue = event.target.elements.search.value.trim();
+  currentPage = 1;
+  hideLoadBtn();
+  hideLoader();
 
   if (!inputValue) {
-    createMessage('Oops', 'You forgot to enter a search query!');
+    createMessage('Oops', 'You forgot to enter a search query!', '#EF4040');
     markupList.innerHTML = '';
   } else {
     showLoader();
     try {
-      const data = await getImage(inputValue);
+      const data = await getImage(inputValue, currentPage);
+      maxPage = data.total / perPage;
       if (data.hits.length === 0) {
         markupList.innerHTML = '';
+        hideLoadBtn();
         createMessage(
           'Sorry',
-          'there are no images matching your search query. Please try again!'
+          'there are no images matching your search query. Please try again!',
+          '#EF4040'
         );
       }
       const markup = renderMarkup(data.hits);
       markupList.innerHTML = markup;
+
+      showLoadBtn();
       lightbox.refresh();
     } catch (err) {
       err => {};
     }
     hideLoader();
     searchForm.reset();
+    updateBtnStatus();
   }
 });
 
-function createMessage(title, message) {
+btnLoadmore.addEventListener('click', async e => {
+  e.preventDefault();
+  currentPage++;
+  hideLoadBtn();
+  showLoader();
+  try {
+    const data = await getImage(inputValue, currentPage);
+    const markup = renderMarkup(data.hits);
+    markupList.insertAdjacentHTML('beforeend', markup);
+    scrollElements();
+  } catch (err) {
+    err => {};
+  }
+
+  lightbox.refresh();
+  hideLoader();
+  updateBtnStatus();
+});
+
+function createMessage(title, message, bckgColor) {
   iziToast.error({
     title: title,
     message: message,
-    backgroundColor: '#EF4040',
+    backgroundColor: bckgColor,
     theme: 'dark',
     position: 'topRight',
   });
@@ -63,4 +97,38 @@ function showLoader() {
 
 function hideLoader() {
   loader.classList.add('is-hidden');
+}
+
+function showLoadBtn() {
+  btnLoadmore.classList.remove('is-hidden');
+}
+
+function hideLoadBtn() {
+  btnLoadmore.classList.add('is-hidden');
+}
+
+function updateBtnStatus() {
+  if (currentPage >= maxPage) {
+    hideLoadBtn();
+
+    if (maxPage) {
+      createMessage(
+        `We're sorry,`,
+        `but you've reached the end of search results.`,
+        '#FFA000'
+      );
+    }
+  } else {
+    showLoadBtn();
+  }
+}
+
+function scrollElements() {
+  const liElem = markupList.children[0];
+  const height = liElem.getBoundingClientRect().height;
+
+  scrollBy({
+    top: height * 2 + 50,
+    behavior: 'smooth',
+  });
 }
